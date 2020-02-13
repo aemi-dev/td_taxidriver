104f7007b5f95c53bf6a51d763c8b3b3abc93bb2
DridriLaBastos <DridriLaBastos@users.noreply.github.com>
2020-02-13 12:15:46 +0100

    Quelques graphique

--- extractionDeDonnee.py
+++ extractionDeDonnee.py
@@ -3,6 +3,7 @@
 # %%
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # %%
@@ -19,7 +20,8 @@ df.drop_duplicates(inplace=True)
 #Il doit y avoir un moyen de le faire directement dans la lecture du fichier mais je ne l'ai pas trouvé. Il y a le
 #paramètre <names> pour la fonction read_csv qui prend la liste des colonnes à lire, mais quand je l'utilise, le résultat
 #que j'obtient est bizarre. Pour l'instant ça, ça marche
-df.drop(['id', 'vendor_id','dropoff_datetime','store_and_fwd_flag'],axis=1,inplace=True)
+#Cette n'est même pas vraiment utile
+#df.drop(['id', 'vendor_id','dropoff_datetime','store_and_fwd_flag'],axis=1,inplace=True)
 
 #Convertion des dates dans un format normalisé
 df['pickup_datetime'] = pd.to_datetime(df.pickup_datetime)
@@ -29,6 +31,38 @@ df.loc[:, "date"] = df.pickup_datetime.dt.date
 df.loc[:, "time"] = df.pickup_datetime.dt.time
 df.loc[:, "dayOfTheWeek"] = df.pickup_datetime.dt.dayofweek
 df.loc[:, "WeekOfTheYear"] = df.pickup_datetime.dt.weekofyear
+
+
+# %%
+#Diagramme affichant le temps des voyages
+#On prend le log car sinon les valeur sont trop grandes
+plt.hist(np.log(df.trip_duration.values), bins=100)
+plt.xlabel('log(trip_duration)')
+plt.show()
+
+
+# %%
+#Diagramme affichant le nombre de voyage par jour de la semaine
+plt.plot(df.groupby('dayOfTheWeek').count()[['id']],'o-')
+plt.xlabel("day of the week (0=Monday...)")
+plt.ylabel("trip count")
+plt.show()
+
+
+# %%
+#Diagramme affichant les lieux de départ
+plt.plot(df.pickup_latitude,df.pickup_longitude,'ro')
+plt.xlabel("pickup latitude")
+plt.ylabel("pickup longitude")
+plt.show()
+
+
+# %%
+#Diagramme affichant le nombre de départ en fonction de l'heure de la journée
+plt.plot(df.groupby(df.pickup_datetime.dt.hour.values).count()[['id']],'o-')
+plt.xlabel("pickup date")
+plt.ylabel("trip count")
+plt.show()
 
 
 # %%
