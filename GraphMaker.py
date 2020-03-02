a22b09961b71d5127acb4f8fe9f97c2ff9f5671a
ThomasCalonge <thomascroblin@outlook.fr>
2020-03-02 16:55:41 +0100

    Cluster!

--- GraphMaker.py
+++ GraphMaker.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
 import csv
+import pandas as pd
 
 x=[]
 y=[]
@@ -14,11 +17,22 @@ with open('train.csv', 'r') as csvfile:
             x.append(float(row[5]))
             y.append(float(row[6]))
         count = count+1
-        if (count>50000):
+        if (count>5000):
             break
 
+data = []
+inc = 0
 
-plt.scatter(x,y, marker='o',s=1)
+for i in x:
+    h = y[inc]
+    v = x[inc]
+    data.append([v,h])
+    inc += 1
+
+
+kmeans = KMeans(n_clusters=15, random_state=2, n_init = 10).fit(data)
+
+plt.scatter(x,y, marker='o',s=1,c=kmeans.labels_)
 
 plt.title('Pickup localisation')
 
